327a71054176eb7e9680caa8a7282098c38a1d25
fwurster <fwurster@stud.hs-heilbronn.de>
2017-12-19 15:13:55 +0100

    Add leaflet map

--- client/src/app/user/task/location/location-map-popup/location-map-popup.component.ts
+++ client/src/app/user/task/location/location-map-popup/location-map-popup.component.ts
@@ -1,5 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+
+declare const L; 
 
 @Component({
   selector: 'app-user-location-map-popup',
@@ -11,7 +13,11 @@ export class UserLocationMapPopupComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<UserLocationMapPopupComponent>,@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
+    console.log("L:", L);
+    const map = L.map('map', { scrollWheelZoom: false}).setView([49.1226, 9.211], 18);
 
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
   }
-
 }
