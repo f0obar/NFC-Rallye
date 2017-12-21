19057ee6f28f53be10b5a930cf992e1e11d60241
fwurster <fwurster@stud.hs-heilbronn.de>
2017-12-21 22:25:04 +0100

    Test wrapper

--- server/routes/adminConfigApi.js
+++ server/routes/adminConfigApi.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const bcrypt = require('bcryptjs');
-
 const authenticator = require('./authMiddleware');
-
 const Config = require('../models/config');
 
 const allowedKeys = {
